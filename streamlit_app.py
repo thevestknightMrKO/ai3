c4da3c4ca85d7ba0fc864467d141a0a536dddef7
thevestknightMrKO <h098s02708@chunchon.gwe.hs.kr>
2025-11-11 11:18:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["아스카보다 레이가 좋습니다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/watch?v=eppqcuMJchQ&list=RDeppqcuMJchQ&start_radio=1"]
      },
 }
 
